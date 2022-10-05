8e4768d84c56846f0422c3a9dc8e6f717c9849b3
sean25xiao <sean.xyb@icloud.com>
2022-10-05 10:49:09 -0400

    update

--- meyers_singleton.cpp
+++ meyers_singleton.cpp
@@ -6,12 +6,24 @@ class A
 {
 public:
   static A& getInstance();
-  compute()
+
+  void compute() { num++; };
+  int get_num() const { return num; }
 
 private:
-  A();
+  A() {num = 1;};
   A(const A& rhs);
-}
+
+  /*
+   *   meyers_singleton.cpp:32:20: error: 'compute' is a private member of 'A'
+   *    A::getInstance().compute();
+   *
+  */
+  //void compute() { num++; };  // can't put here
+  //int get_num() const { return num; }
+
+  int num;
+};
 
 A& A::getInstance() {
 	static A a;          // Create a static self
@@ -21,4 +33,17 @@ A& A::getInstance() {
 int main()
 {
   cout << "hello!" << endl;
+
+  cout << A::getInstance().get_num() << endl;
+  cout << A::getInstance().get_num() << endl;
+  A::getInstance().compute();
+  cout << A::getInstance().get_num() << endl;
 }
+
+// Output:
+/*
+hello!
+1
+1
+2
+*/
